a042db95612313a3d5ba2ba48c1f072626a9e519
Elanora Manson <git@elanora.lol>
2025-11-22 02:34:55 -0800

    Node 25 support, tree-sitter lang 15

--- src/binding.cc
+++ src/binding.cc
@@ -8,19 +8,19 @@ extern "C" TSLanguage * tree_sitter_fluent();
 
 namespace {
 
-NAN_METHOD(New) {}
+NAN_METHOD(New) {};
 
 void Init(Handle<Object> exports, Handle<Object> module) {
   Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
   tpl->SetClassName(Nan::New("Language").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  Local<Function> constructor = tpl->GetFunction();
+  Local<Function> constructor = tpl->GetFunction(Nan::GetCurrentContext()).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_fluent());
 
-  instance->Set(Nan::New("name").ToLocalChecked(), Nan::New("fluent").ToLocalChecked());
-  module->Set(Nan::New("exports").ToLocalChecked(), instance);
+  instance->Set(Nan::GetCurrentContext(), Nan::New("name").ToLocalChecked(), Nan::New("fluent").ToLocalChecked()).Check();
+  module->Set(Nan::GetCurrentContext(), Nan::New("exports").ToLocalChecked(), instance).Check();
 }
 
 NODE_MODULE(tree_sitter_fluent_binding, Init)
